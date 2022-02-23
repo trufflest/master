cc19eed8dbbe5d0f6cb9e5dc2189f415e958b13d
vaux <zero.griffin@gmail.com>
2022-02-23 13:23:44 +0100

    Update face

--- Sources/Face.swift
+++ Sources/Face.swift
@@ -1,8 +1,19 @@
-public enum Face: Hashable {
+public enum Face: Equatable {
     case
     none,
     jump,
     dead,
     walk1(Int),
     walk2(Int)
+    
+    public var key: String {
+        switch self {
+        case .walk1:
+            return "walk1"
+        case .walk2:
+            return "walk2"
+        default:
+            return "\(self)"
+        }
+    }
 }
